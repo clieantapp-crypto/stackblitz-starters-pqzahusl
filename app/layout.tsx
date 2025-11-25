a0b7a02ba51d01bb73f450f3098c04433897be1c
clieantapp-crypto <clieantapp@gmail.com>
2025-11-25 14:34:46 +0300

    s

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: ' الخدمات وآخر العروض الحصرية',
-  description: 'اكتشف    الخدمات وآخر العروض عبر روابطنا ',
+  description: 'اكتشف الخدمات وآخر العروض عبر روابطنا ',
 
 };
 
